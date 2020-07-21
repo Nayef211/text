862946409c958c7ad9e8136fd0e0ac6dcdfa2d7d
Nayef Ahmed <nayef.ahmed211@gmail.com>
2020-07-20 17:07:45 -0700

    performing becnhmarking

--- benchmark/experimental_vocab.py
+++ benchmark/experimental_vocab.py
@@ -10,8 +10,9 @@ from torchtext.vocab import Vocab
 def benchmark_experimental_vocab():
     def _run_benchmark_lookup(tokens, vocab):
         t0 = time.monotonic()
-        for token in tokens:
-            vocab[token]
+        for _ in range(5):
+            for token in tokens:
+                vocab[token]
         print("Lookup time:", time.monotonic() - t0)
 
     train, = AG_NEWS(data_select='train')
@@ -36,7 +37,7 @@ def benchmark_experimental_vocab():
     t0 = time.monotonic()
     v_experimental = VocabExperimental(ordered_dict)
     print("Construction time:", time.monotonic() - t0)
-    jit_v_experimental = torch.jit.script(v_experimental)
+    # jit_v_experimental = torch.jit.script(v_experimental)
 
     # # existing Vocab not jit lookup
     # print("Vocab - Not Jit Mode")
@@ -46,9 +47,9 @@ def benchmark_experimental_vocab():
     print("Vocab Experimental - Not Jit Mode")
     _run_benchmark_lookup(tokens, v_experimental)
 
-    # experimental Vocab jit lookup
-    print("Vocab Experimental - Jit Mode")
-    _run_benchmark_lookup(tokens, jit_v_experimental)
+    # # experimental Vocab jit lookup
+    # print("Vocab Experimental - Jit Mode")
+    # _run_benchmark_lookup(tokens, jit_v_experimental)
 
 
 if __name__ == "__main__":
